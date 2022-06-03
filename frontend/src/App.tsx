29bc30b75e12bcfd3b50ba8010ea6a56c9861493
Alina Mailach <mailach@informatik.uni-leipzig.de>
2022-06-03 23:06:22 +0200

    Show sample data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,41 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Box, Typography } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [events, setEvents] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const fetchData = () => {
+    fetch(`http://localhost:3000`)
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setIsError(true);
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <Box sx={{ display: "flex", align: "center", flexDirection: "column" }}>
+      <Typography variant="h1">Termine:</Typography>
+      {events && (
+        <Typography variant="body1">{JSON.stringify(events)}</Typography>
+      )}
+      {isError && <div>Error fetching data.</div>}
+    </Box>
+  );
 }
 
-export default App
+export default App;
