ee2b1b783f013cdca5c17c1b4affe7c1da365ade
Paul Reinhardt <paul.reinhardt@studserv.uni-leipzig.de>
2022-06-06 13:04:14 +0200

    Manually add apiProperty to CreateEventDto

--- backend/src/events/dto/create-event.dto.ts
+++ backend/src/events/dto/create-event.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateEventDto {
+  @ApiProperty()
   title: string;
+
+  @ApiProperty()
   date: Date;
+
+  @ApiProperty()
   maxParticipants: number;
 }
