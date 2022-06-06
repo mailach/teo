18f95bf7facbc3ed91671d2b23ff70eafad360a8
Paul Reinhardt <paul.reinhardt@studserv.uni-leipzig.de>
2022-06-06 13:16:10 +0200

    Use map for event rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Box, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 
 function App() {
-  const [events, setEvents] = useState(undefined);
+  const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const fetchData = () => {
@@ -30,9 +30,12 @@ function App() {
   return (
     <Box sx={{ display: "flex", align: "center", flexDirection: "column" }}>
       <Typography variant="h1">Termine:</Typography>
-      {events && (
-        <Typography variant="body1">{JSON.stringify(events)}</Typography>
-      )}
+      {events &&
+        events.map((event: any) => (
+          <Card key={event.id} sx={{ padding: "8px" }}>
+            <Typography variant="body1">{JSON.stringify(event)}</Typography>
+          </Card>
+        ))}
       {isError && <div>Error fetching data.</div>}
     </Box>
   );
